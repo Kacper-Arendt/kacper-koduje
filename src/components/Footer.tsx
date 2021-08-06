8adb8f628079a1e0d17c410bb9e2d98d52feab06
kacper <kacykvaa@gmail.com>
2021-08-06 14:37:49 +0200

    refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,32 +2,73 @@ import styled from "styled-components"
 
 const StyledFooter = styled.footer`
   display: flex;
-  height: 7rem;
-  align-items: center;
+  flex-wrap: wrap;
   justify-content: space-between;
-  background-color: rgba(0, 0, 0, .8);
-  text-align: center;
+  align-items: baseline;
+  margin-top: auto;
+  padding: 2.5rem;
+  background-color: #1F3A60;
+  white-space: nowrap;
+  
+  p{
+    margin: .7rem 0;
+    color:  #C5C5C5;
+  }
+`
 
+const Contact = styled.div`
+  p:first-child{
+    color: white;
+    font-weight: bold;
+  }
+`
+
+const Social = styled.div`
   p {
-    padding: 0 1rem;
+    :first-of-type{
+      font-weight: bold;
+      color: white;
+    }
   }
 
   a {
     text-decoration: none;
-    color: orange;
+    color: white;
+    transition: all .3s;
+    :hover {
+      color: orange;
+  }
   }
 `
 
+const Copyright = styled.p`
+    flex: 1 100%;
+    text-align: start;
+`
+
+const Line = styled.div`
+  width: 100%;
+  height: .2rem;
+  margin: 1rem 0;
+  border-bottom: 1px solid #C5C5C5;
+`
 export const Footer = () => {
     return (
         <StyledFooter>
-            <p>KacperKoduje.pl &copy; 2021</p>
-            <p>
-                Ikonki wykonane przez
-                <a href="https://www.freepik.com" title="Freepik"> Freepik </a>
-                |
-                <a href="https://www.flaticon.com/" title="Flaticon"> flaticon</a>
-            </p>
+            <Contact>
+                <p>Kacper Arendt</p>
+                <p>arendtkacper@gmail.com</p>
+                <p>721-446-865</p>
+                <p>Warsaw</p>
+            </Contact>
+            <Social>
+                <p>Find Me</p>
+                <a href="https://github.com/kacykvaa" target="_blank" rel="noopener noreferrer">
+                    Github
+                </a>
+            </Social>
+            <Line />
+            <Copyright>KacperKoduje.pl &copy; 2021</Copyright>
         </StyledFooter>
     )
 }
