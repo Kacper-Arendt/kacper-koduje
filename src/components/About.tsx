cfad718b077242e71c085d7aa1cc9b11f1573ee2
kacper <kacykvaa@gmail.com>
2021-08-09 10:48:43 +0200

    style about add media for higher resolutions

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,16 +4,18 @@ import {device} from "../Models/MediaQueries";
 import {me} from '../images/Images'
 
 const Div = styled(StyledWrapper)`
+  margin: 5rem 0;
   h2 {
     width: 80%;
     margin-top: 2rem;
   }
 
 @media${device.tablet} {
+  margin: 10rem 0;
   display: grid;
-  max-width: 150rem;
-  grid-template-rows: 10rem min-content;
-  grid-template-columns: 1fr minmax(55%, 30rem) minmax(35%, 15rem) 1fr;
+  max-width: 110rem;
+  grid-template-rows: 10rem fit-content(50vh);
+  grid-template-columns: 1fr minmax(60%, 30rem) minmax(25%, 15rem) 1fr;
   grid-template-areas: 
       ". header . ."
       ". text img .";
@@ -23,9 +25,13 @@ const Div = styled(StyledWrapper)`
     grid-area: header;
   }
 }
+@media${device.laptopL} {
+  margin: 13rem 0;
+}
 `
 
 const Text = styled.div`
+  width: 80%;
   display: flex;
   flex-direction: column;
   color: black;
@@ -40,6 +46,10 @@ const Text = styled.div`
       margin: 0;
     }
   }
+
+@media${device.tablet} {
+  width: 100%;
+}
 `
 const Image = styled.div`
   grid-area: img;
